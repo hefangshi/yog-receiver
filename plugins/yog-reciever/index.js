ebb56bf97898a5d291da9801ddfa66a75ece2ab0
Torrent Lee <torrent88410@gmail.com>
2015-01-15 15:57:23 +0800

    update

--- plugins/yog-reciever/index.js
+++ plugins/yog-reciever/index.js
@@ -3,29 +3,50 @@ var fs = require('fs');
 var cp = require('child_process');
 var multer = require('multer');
 var os = require('os');
+var async = require('async');
 
 var receiver = module.exports['yog-receiver'] = function( app, conf ){
   console.log('hook receiver start');
   app.post('/receiver',multer(conf));
   app.post('/receiver',function( req, resp, next ) {
+    if( !req.body 
+      || !req.body.to 
+      || !req.files
+      || Object.keys(req.files).length 
+    ){
+      return next('illegal request');
+    }
+
     var body = req.body;
     
-    yog.log.debug( '[receiver] copy file root', conf.root);
-    yog.log.debug( '[receiver] copy file to', body.to);
+    yog.log.debug( '[receiver] copy file root' + conf.root);
+    yog.log.debug( '[receiver] copy file to' + body.to);
+
     var to = path.join( conf.root, body.to );
-    yog.log.debug( '[receiver] copy file to', to);
+    yog.log.debug( '[receiver] copy file to' + to);
+
     cp.exec( conf.cmd + path.dirname( to ),
       function( e ) {
         if( e ){
           yog.log.fatal( e );
         }
-        var ws = fs.createWriteStream( to );
-        console.log( req.files );
-        ws.end( req.files[0].buffer );
-        ws.on('finish',function() {
-          yog.log.debug('[receiver] copy end ', to);
-          req.end(0);
-        });
+        async.each(Object.keys(req.files),
+          function( file, done) {
+          
+            var ws = fs.createWriteStream( to );
+            ws.end( req.files[file].buffer );
+          
+            ws.on('finish',function() {
+              yog.log.debug('[receiver] copy end ' + to);
+              done();
+            });
+            ws.on('error',function( e ) {
+              done(e);
+            });
+          },
+          function( e ) {
+            req.end(null,e ? 0 : 1);
+          });
       });
   });
 };
--- test/test_reciever.js
+++ test/test_reciever.js
@@ -2,14 +2,16 @@ var request = require('request');
 var fs = require('fs');
 
 request.post('http://dev073.baidu.com:8480/receiver',{
-  custom_file: {
-    value:  fs.createReadStream('../package.json'),
-    options: {
-      filename: 'package.json',
-      contentType: 'image/jpg'
-    }
-  },
-  to : './temp/package.json'
+  formData : {
+    custom_file: {
+      value:  fs.createReadStream('../package.json'),
+      options: {
+        filename: 'package.json',
+        contentType: 'image/jpg'
+      }
+    },
+    to : './temp/package.json'
+  }
 },function( err, resp, body ) {
   console.log( body );
 });
