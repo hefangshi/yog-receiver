4e73db962d363774d4f631be7ab19faa78561290
Torrent Lee <torrent88410@gmail.com>
2015-01-15 14:36:40 +0800

    update

--- plugins/yog-reciever/index.js
+++ plugins/yog-reciever/index.js
@@ -1,28 +1,35 @@
-var reciever = module.exports['yog-reciever'] 
-= function( app, conf ){
+var path = require('path');
+var fs = require('fs');
+var cp = require('child_process');
+var multer = require('multer');
+var os = require('os');
 
-  var fs = require('fs');
-  var cp = require('child_process');
-  var path = require('path');
-  var multer = require('multer');
-
+var receiver = module.exports['yog-receiver'] = function( app, conf ){
   return function(){
-    app.post('/reciever',multer(conf));
-    app.post('/reciever',function( req, resp, next ) {
+    app.post('/receiver',multer(conf));
+    app.post('/receiver',function( req, resp, next ) {
       var body = req.body;
-      var to = body.to;
-      cp.exec('mkdir ' + path.dirname(to),function( e ) {
-        var ws = fs.createWriteStream( to );
-        ws.write( req.files[0].buffer );
-        ws.on('drain',function() {
-          req.end(0);
-        });
-      })
+      var to = path.join( conf.root, body.to );
+      yog.log.debug( '[receiver] copy file to', to);
+      cp.exec( conf.cmd + path.dirname( to ),
+        function( e ) {
+          if( e ){
+            yog.log.fatal( e );
+          }
+
+          var ws = fs.createWriteStream( to );
+          ws.write( req.files[0].buffer );
+
+          ws.on('drain',function() {
+            req.end(0);
+          });
+        })
     });
   }
 };
 
-recierver.defaultConf = {
+receiver.defaultConf = {
   inMemory : true,
-  root     : 
+  root     : path.join(__dirname,'../../'),
+  cmd      : (os.platform() == 'win32' ? 'mkdir' : 'mkdir -p') + ' '
 };
